f87c3da3a0b2d228cb4e73b4386b94b1a95fd0d0
unknown <santiago.ospina-m@labinfo.is.escuelaing.edu.co>
2023-11-09 12:48:06 -0500

    taller terminado

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import AgentType, initialize_agent
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms.openai import OpenAI
 from langchain.tools import tool
+from langchain.vectorstores.pinecone import Pinecone
+import pinecone
 
 
 @tool("SayHello", return_direct=True)
@@ -8,10 +13,21 @@ def say_hello(name: str) -> str:
     """Answear when someone says hello"""
     return f"Hello {name}! my name is Sainapsis"
 
+@tool("programInfo", return_direct=True)
+def buscarInformacion(query: str) -> str:
+    """responde cuando busca informacion de los documentos"""
+    pinecone.init(api_key="c62ba564-0b2a-4d84-8ab7-41d1b7618444", environment="gcp-starter")
+    embeddings = OpenAIEmbeddings()
+    docsearch = Pinecone.from_existing_index("taller", embeddings)
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="refine", retriever=docsearch.as_retriever(tipo="similarity"))
+    query = "cuantos años de acreditación tiene ingeniería electrica?"
+    return qa.run(query)
+
 def main():
     llm = ChatOpenAI(temperature=0)
     tools = [
-        say_hello
+        say_hello,
+        buscarInformacion
     ]
     agent = initialize_agent(
         tools=tools,
@@ -19,7 +35,7 @@ def main():
         agent=AgentType.OPENAI_FUNCTIONS,
         verbose=True
     )
-    print(agent.run("hello! my name is Santiago"))
+    print(agent.run("el programa de ingenieria de istemas tiene acreditacion"))
 
 if __name__ == '__main__':
     main()
